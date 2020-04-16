fb5e58891569f20d34e20251f07e6e2342af0051
Komal Sajnani <ksajnani@in.ibm.com>
2020-04-16 21:01:50 +0530

    my first Python program

---
+++ fabonacci.py
@@ -0,0 +1,15 @@
+num = input("Enter the number for which you want to print the series: ")
+x = 0
+y = 1
+i=0
+print(y)
+while i<=int(num):
+    z=x+y
+    print(z)
+    x=y
+    y=z
+    i+=1
+
+
+
+
